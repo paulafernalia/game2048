change qlearning_action to eps_greedy_action; implement model training optimizing on one action output; fix bug argmax to max in target calculation; add huber loss

agents.py
@@ -1,17 +1,18 @@
-from tensorflow import keras
-from keras.models import Sequential, clone_model
+from keras.models import Sequential, clone_model, Model
 from keras.optimizers import Adam
-from keras.layers import Dropout, Dense
+from keras.layers import Dropout, Dense, Lambda, Input
+from keras import backend as K
 import numpy as np
 
 
 class DeepQ_MLP():
 
-    def __init__(self, input_shape, epsilon=.05, learning_rate=0.01, dropout=.1,
-                 discount_epsilon=1., discount_factor=1., c=100, beta_1=.9,
-                 warmup=50000, verbose=True):
+    def __init__(self, input_shape, epsilon=.05, learning_rate=0.01,
+                 dropout=.1, discount_epsilon=1., discount_factor=1.,
+                 c=100, beta_1=.9, warmup=50000, verbose=True):
 
         self.actions = ['D', 'U', 'L', 'R']
+        self.num_actions = len(self.actions)
         self.gamma = discount_factor
         self.discount_epsilon = discount_epsilon
         self.c = c
@@ -20,6 +21,7 @@ class DeepQ_MLP():
         self.init_epsilon = epsilon
         self.construct_network(input_shape, dropout, learning_rate, beta_1)
         self.update_target_network()
+        self.construct_trainable_model(learning_rate, beta_1)
         self.reset()
 
     def reset(self):
@@ -46,7 +48,7 @@ class DeepQ_MLP():
         self.Qmodel.add(Dense(128, activation='relu'))
         self.Qmodel.add(Dropout(rate=dropout))
         self.Qmodel.add(Dense(64, activation='relu'))
-        self.Qmodel.add(Dense(len(self.actions), activation='linear'))
+        self.Qmodel.add(Dense(self.num_actions, activation='linear'))
 
         opt = Adam(learning_rate=learning_rate, beta_1=beta_1)
         self.Qmodel.compile(loss='mse', optimizer=opt)
@@ -59,7 +61,7 @@ class DeepQ_MLP():
         # target network is never compiled
         self.target_Qmodel.compile(loss='mse', optimizer=Adam())
 
-    def qlearning_action(self, phi, tabu):
+    def eps_greedy_action(self, phi, tabu):
         """Predict movement of game controler where is epsilon
         probability randomly move."""
 
@@ -91,11 +93,9 @@ class DeepQ_MLP():
         if len(tabu) > 0:
             if randn < self.epsilon:
                 aval_actions = [a for a in self.actions if a not in tabu]
-                print(tabu)
-                print(aval_actions)
             else:
                 # Update Qs to low values to ensure they are not picked
-                tabu_idx = [i for i in range(len(self.actions)) if self.actions[i] in tabu]
+                tabu_idx = [i for i in range(self.num_actions) if self.actions[i] in tabu]
                 qvalues[tabu_idx] = -9999
 
         # eps-greedy, select random action
@@ -119,37 +119,58 @@ class DeepQ_MLP():
             uses the target network to make predictions
         """
         # initialise array to store yj values
-        target = np.zeros(len(batch[0]))
+        target = np.zeros((len(batch[0]), self.num_actions))
 
         # loop over samples in the minibatch
         for j in range(len(batch[0])):
 
+            a0_i = self.action_str2idx(batch[1][j])
+            r0 = batch[2][j]
+            done = batch[3][j]
+            s1 = batch[4][j]
+
             # if terminating state
-            if batch[3][j]:
-                target[j] = batch[2][j]
+            if done:
+                target[j, a0_i] = r0
             else:
-                q_target = self.target_Qmodel.predict(batch[4][j])
-                target[j] = batch[2][j] + self.gamma * np.argmax(q_target)
+                qs_target = self.target_Qmodel.predict(s1)
+                target[j, a0_i] = r0 + self.gamma * np.max(qs_target)
 
         return target
+
+    def get_masks(self, actions):
+        masks = np.zeros((len(actions), self.num_actions))
+
+        for j in range(len(actions)):
+
+            a0_i = self.action_str2idx(actions[j])
+            masks[j, a0_i] = 1.
+
+        return masks
 
     def one_step_gd(self, batch):
         """ Perform one step of gradient descent on (yj - Q(phi, aj, w))^2 """
 
         # get target values yj
-        Y = self.get_target(batch)
-        X = np.vstack(batch[0])
+        targets = self.get_target(batch)
+        phi_input = np.vstack(batch[0])
+        masks = self.get_masks(batch[1])
+        dummy_targets = targets.max(axis=1)
+
+        X = [phi_input, targets, masks]
+        Y = [dummy_targets, targets]
 
         # update main network with one step of gradient descent
-        self.Qmodel.fit(X, Y, batch_size=len(X))
+        # self.Qmodel.fit(X, Y, batch_size=len(X))
+        metrics = self.train_model.train_on_batch(X, Y)
 
         # every fixed number of steps, update target network
         self.c_count += 1
+        # print(self.c_count, self.c)
 
         if self.c_count == self.c:
-
             if self.verbose:
-                print('target network updated')
+                print('* Target network updated')
 
             # update target network to be equal the main network
             self.update_target_network()
@@ -159,3 +180,32 @@ class DeepQ_MLP():
 
     def action_str2idx(self, action):
         return np.argwhere(np.array(self.actions) == action)[0][0]
+
+    def construct_trainable_model(self, learning_rate, beta_1):
+
+        def masked_error(args):
+            y_true, y_pred, mask = args
+            loss = K.square(y_true - y_pred)
+            loss *= mask
+            return K.sum(loss, axis=-1)
+
+        y_true = Input(name='y_true', shape=(self.num_actions,))
+        mask = Input(name='mask', shape=(self.num_actions,))
+        y_pred = self.Qmodel.output
+
+        loss_out = Lambda(masked_error,
+                          output_shape=(1, ),
+                          name='loss')([y_true, y_pred, mask])
+
+        train_model = Model([self.Qmodel.input] + [y_true, mask],
+                            outputs=[loss_out])
+
+        # loss is computed in Lambda layer
+        losses = [
+            lambda y_true, y_pred: y_pred,
+            # lambda y_true, y_pred: K.zeros_like(y_pred)
+        ]
+        opt = Adam(learning_rate=learning_rate, beta_1=beta_1)
+        train_model.compile(optimizer=opt, loss=losses)
+
+        self.train_model = train_model