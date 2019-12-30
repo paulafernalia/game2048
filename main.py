0077309b14e2efbfa0aebc780a4126b3d3e0b245
paulafernalia <paula.fermin.cueto@gmail.com>
2019-12-30 19:37:02 +0100

    add printing and pickle dumping - not working

--- main.py
+++ main.py
@@ -2,21 +2,23 @@ from utils import ReplayBuffer
 from agents import DeepQ_MLP
 from environments import Game2048
 from matplotlib.animation import FFMpegWriter
+import pdb
+import pickle
 
 # import cProfile
 
-BUFFER_SIZE = 1000000
-WARMUP_ACTIONS = 50000
-C_TARGET_UPDATE = 10000
+BUFFER_SIZE = 500000
+WARMUP_ACTIONS = 2  # 25000  # 50000
+C_TARGET_UPDATE = 5000
 MINIBATCH_SIZE = 32
-LEARNING_RATE = .0005
-DISCOUNT_FACTOR = .99999
-EPSILON = .5
-DISCOUNT_EPSILON = .9995
+LEARNING_RATE = .005
+DISCOUNT_FACTOR = .9999
+EPSILON = .1
+DISCOUNT_EPSILON = 1  # .9995
 BETA_1 = .95
-PLOT_FREQ = 50
-PRINT_FREQ = 1
-MAX_ITER = 20
+PLOT_FREQ = 1000
+PRINT_FREQ = 100
+MAX_ITER = 10000
 
 
 def main():
@@ -42,8 +44,11 @@ def main():
     # Run a number of episodes
     for episode in range(MAX_ITER):
 
+        # pdb.set_trace()
+
         if (episode % PLOT_FREQ == 0) and (agent.a_count >= agent.warmup):
-            print('check')
+            print()
+            print('* Plot this episode {}'.format(episode))
             plot_this_episode = True
             env.render(iter_=episode)
             writer.setup(fig=env.fig, dpi=300,
@@ -59,7 +64,7 @@ def main():
             # take one action -> next step and reward
             # this action will be random with probability epsilon
             # and always random before warmup
-            action, qmax = agent.qlearning_action(phi, env.tabu)
+            action, qmax = agent.eps_greedy_action(phi, env.tabu)
             reward = env.step(action)
 
             # add this tuple (s0, a, r, d, s1) to the memory
@@ -68,7 +73,8 @@ def main():
             # if within the initial replay buffer start, do nothing
             if agent.a_count >= agent.warmup:
 
-                print('check')
+                pdb.set_trace()
+
                 # sample random minibatch of transitions
                 minibatch = memory.sample(MINIBATCH_SIZE)
 
@@ -90,8 +96,12 @@ def main():
 
         # # print progress
         if (episode % PRINT_FREQ == 0):
+            print()
             print("* Episode: {} score: {} actions: {}".format(
                   episode, int(env.score), agent.a_count))
+            # print(agent.c_count)
+            print('.   - main model  ', agent.Qmodel.get_weights()[0][0, 0])
+            print('.   - target model', agent.target_Qmodel.get_weights()[0][0, 0])
 
         # # close moviewriter
         if plot_this_episode and (agent.a_count >= agent.warmup):
@@ -100,7 +110,13 @@ def main():
         # reset environment 2048
         env.reset()
 
+        # pdb.set_trace()
+        if env.done:
+            pdb.set_trace()
+
+    with open('results.pickle', 'wb') as f:
+        pickle.dump(progress, f)
+
 
 if __name__ == "__main__":
     main()
-    # cProfile.run('main()') 
